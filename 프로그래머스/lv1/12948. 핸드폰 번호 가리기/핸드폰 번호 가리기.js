d852b1b3714a4cb6ec86bef9f2d204dad00e614f
sleepy-joyy <115705457+sleepy-joyy@users.noreply.github.com>
2022-11-08 22:20:05 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -8,3 +8,11 @@ function solution(phone_number) {
     lastno = phone_number.slice(-4);
     return `${star}${lastno}`;
 }
+
+
+//별모양을 누적해서 담을 변수 선언
+//마지막 4자리 숫자를 담을 변수선언
+
+//for문을 돌면서 전체 폰넘어 길이의 -4만큼 * 누적
+//slice를 통해 뒤에서부터 4자리를 가져옴
+//템플릿으로 이어주고 리턴
