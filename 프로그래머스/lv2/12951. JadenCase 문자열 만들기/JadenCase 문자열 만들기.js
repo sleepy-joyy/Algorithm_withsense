8fbd131bf216e5e4b6cc614a02076b3849b6eef9
sleepy-joyy <115705457+sleepy-joyy@users.noreply.github.com>
2022-12-14 20:39:52 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -4,3 +4,14 @@ function solution(s) {
         .map((v) => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase())
         .join(" ");
 }
+
+/* 공백을 기준으로 스플릿한 배열을 만들고
+배열의 각각의 요소들을
+
+0번째 글자는 대문자로 --> v[0].toUpperCase() 하면 채점시 런타임에러남 싱기...이건 왜안돼?
+
+나머지문자들은 1번인덱스 부터 substring 해서 
+부분 문자열을 반환한값에 소문자화
+
+나중에 join " "
+*/
