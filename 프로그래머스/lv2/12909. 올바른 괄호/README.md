bad49d5fb19b1841ad54771864517e5e3ee8fa3e
sleepy-joyy <115705457+sleepy-joyy@users.noreply.github.com>
2022-12-19 18:42:52 +0900

    [level 2] Title: 올바른 괄호, Time: 2.92 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/README.md
+++ 프로그래머스/lv2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 37 MB, 시간: 2.61 ms
+메모리: 37 MB, 시간: 2.92 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,21 +1,25 @@
-function solution(s){
-    let pair = 0;
+// function solution(s){
+//     let pair = 0;
 
-    for(let i=0; i<s.length; i++) {
-        if(s[i] === '(') pair++;
-        else pair--;
+//     for(let i=0; i<s.length; i++) {
+//         if(s[i] === '(') pair++;
+//         else pair--;
         
-        if(pair<0) {
-            return false
-        }
-    }
-    return pair!==0 ? false : true
-}
+//         if(pair<0) {
+//             return false
+//         }
+//     }
+//     return pair!==0 ? false : true
+// }
     
     
     
+
+
+
+
     
-/* 아래풀이론 채점시 테케 4,5,11,18 실패 _효율성은 통과
+function solution(s){
 
     var answer = true;
     let open = 0;
@@ -24,14 +28,13 @@ function solution(s){
 
     if(s[0]=== ')') { 
         return false 
-        
     } else {
      for(let i=0;i<s.length;i++){
         if(s[i] === "(") open++;
         else close++;
+        if(open < close) return false;
      }
     }
-    return open === close ? true :false;
+    return open !== close ? false :true;
   }
 
-*/
